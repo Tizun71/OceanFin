389d397999a7fac62237b0545b049cd3bc242750
Vo Quang Chien <2giosangmitom@proton.me>
2026-02-14 16:27:03 +0700

    fix: remove dead code

--- backend/apps/src/defi_agent/interfaces/defi_agent.controller.ts
+++ backend/apps/src/defi_agent/interfaces/defi_agent.controller.ts
@@ -2,15 +2,9 @@ import { Body, Controller, Post, Res } from '@nestjs/common';
 import { DefiAgentService } from '../application/defi_agent.service';
 import { type Response } from 'express';
 import { CreateStrategyPromptDTO } from './dto/create_strategy_prompt';
-import { streamText } from 'ai';
-import { createGoogleGenerativeAI } from '@ai-sdk/google';
 
 @Controller('defi-agent')
 export class DefiAgentController {
-  private gemini = createGoogleGenerativeAI({
-    apiKey: process.env.GEMINI_API_KEY,
-  });
-
   constructor(private readonly defiAgentService: DefiAgentService) {}
 
   @Post('create-strategy')
